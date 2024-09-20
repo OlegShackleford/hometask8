f1330e2152e5625a36e7b8ec45b599592516bf6d
OlegNord88 <nordhagen88@rambler.ru>
2024-09-20 21:17:04 +0300

    Update homework 8_3.

--- src/ht8_2/BaseConverter.java
+++ src/ht8_2/BaseConverter.java
@@ -1,23 +1,46 @@
 package ht8_2;
 
 public class BaseConverter {
-    public void convert(double temp,Scale type){
-        if (type.equals(Scale.KELVIN))
-        {
-            double kelvin = temp + 273.15;
-            System.out.println("Температура: " + kelvin+"K");
-
-        } else if (type.equals(Scale.FAHRENHEIT))
-        {
-            double fahrenheit = temp * 1.8 + 32;
-            System.out.println("Температура: " + fahrenheit+"F");
-        } else if(type.equals(Scale.CELSIUS)){
-            System.out.println("Температура: " + temp+"C");
+    public void convert(double temp, Scale type) {
+        switch (type) {
+            case CELSIUS_TO_KELVIN:
+                double ctk = temp + 273.15;
+                System.out.println("Температура: " + ctk + "K");
+                break;
+            case CELSIUS_TO_FAHRENHEIT:
+                double ctf = temp * 1.8 + 32;
+                System.out.println("Температура: " + ctf + "F");
+                break;
+            case KELVIN_TO_FAHRENHEIT:
+                double ktf = (temp - 273.15) * 1.8 + 32;
+                System.out.println("Температура: " + ktf + "F");
+                break;
+            case KELVIN_TO_CELSIUS:
+                double ktc = temp - 273.15;
+                System.out.println("Температура: " + ktc + "F");
+                break;
+            case FAHRENHEIT_TO_CELSIUS:
+                double ftc = (temp - 32) / 1.8;
+                System.out.println("Температура: " + ftc + "C");
+                break;
+            case FAHRENHEIT_TO_KELVIN:
+                double ftk = (temp - 32) / 1.8 + 273.15;
+                System.out.println("Температура: " + ftk + "K");
+                break;
+            default:
+                System.out.println("Oops");
         }
+
+
     }
 
     public enum Scale {
-        KELVIN,FAHRENHEIT,CELSIUS
+        CELSIUS_TO_KELVIN,
+        CELSIUS_TO_FAHRENHEIT,
+        FAHRENHEIT_TO_KELVIN,
+        FAHRENHEIT_TO_CELSIUS,
+        KELVIN_TO_CELSIUS,
+        KELVIN_TO_FAHRENHEIT
     }
 
 }
--- src/ht8_2/Main.java
+++ src/ht8_2/Main.java
@@ -3,8 +3,8 @@ package ht8_2;
 public class Main {
     public static void main(String[] args) {
         BaseConverter baseConverter = new BaseConverter();
-        baseConverter.convert(30, BaseConverter.Scale.FAHRENHEIT);
-        baseConverter.convert(56, BaseConverter.Scale.KELVIN);
-        baseConverter.convert(12, BaseConverter.Scale.CELSIUS);
+        baseConverter.convert(30, BaseConverter.Scale.FAHRENHEIT_TO_CELSIUS);
+        baseConverter.convert(56, BaseConverter.Scale.KELVIN_TO_FAHRENHEIT);
+        baseConverter.convert(12, BaseConverter.Scale.CELSIUS_TO_FAHRENHEIT);
     }
 }
